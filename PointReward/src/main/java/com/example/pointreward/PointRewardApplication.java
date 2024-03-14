f279e382dd04122ddcfd20cdb8759b0561c2312f
jaeilssss <jaeilssss@gmail.com>
2024-03-14 12:15:00 +0900

    init

---
+++ PointReward/src/main/java/com/example/pointreward/PointRewardApplication.java
@@ -0,0 +1,13 @@
+package com.example.pointreward;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointRewardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PointRewardApplication.class, args);
+    }
+
+}
---
+++ PointReward/src/test/java/com/example/pointreward/PointRewardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pointreward;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointRewardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
